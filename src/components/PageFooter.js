84fc1267028cb00c379b95cf6ce86a08adbc158f
Dillon Dugan <dugan_dillon@yahoo.com>
2020-09-13 12:32:01 -0700

    woops

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -7,7 +7,7 @@ export default function PageFooter() {
       <ul className="copyright">
         <li>&copy; {config.authorName}</li>
         <li>
-          Photography: <a href="ryantyko.com">Ryan Ko</a>
+          Photography: <a href="https://www.ryantyko.com">Ryan Ko</a>
         </li>
       </ul>
     </footer>
